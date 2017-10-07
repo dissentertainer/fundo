fac92a586cf7bda484efd1b174aed8ccb6f7ed69
Max Dana <max@dissentertainment.com>
2017-10-07 17:36:45 -0400

    [#151504893] update geocoder initializer to use api key

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -3,6 +3,8 @@ require 'redis'
 Geocoder.configure(
   timeout: 10,
   lookup: :google,
+  use_https: true,
+  api_key: ENV['GOOGLE_API_KEY'],
   cache: Redis.new,
   always_raise: [
     Geocoder::OverQueryLimitError,
