d4b0ddb6c4a02840072182b7fc2d150923c6fcbd
Max Dana <max@dissentertainment.com>
2017-10-08 14:53:16 -0400

    fix redis config

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,11 +1,13 @@
 require 'redis'
 
+uri = URI.parse(ENV["REDISTOGO_URL"])
+
 Geocoder.configure(
   timeout: 10,
   lookup: :google,
   use_https: true,
   api_key: ENV['GOOGLE_API_KEY'],
-  cache: Redis.new,
+  cache: Redis.new(:url => uri),
   always_raise: [
     Geocoder::OverQueryLimitError,
     Geocoder::RequestDenied,
