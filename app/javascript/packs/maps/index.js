3759cdd874fe15f1475ced377361ddd7700b67ee
Max Dana <max@dissentertainment.com>
2018-07-29 18:39:49 -0400

    handle output from ipinfo.io in home page view

--- app/javascript/packs/maps/index.js
+++ app/javascript/packs/maps/index.js
@@ -3,6 +3,17 @@ document.addEventListener("turbolinks:load", function() {
   var loc = JSON.parse(document.querySelector('#map').dataset.location);
   window.loc = loc;
 
+  // handle output from ipinfo.io results
+
+  if (loc) {
+    if (loc.latitude == undefined) {
+      loc.latitude = loc.loc.split(',')[0];
+    }
+    if (loc.longitude == undefined) {
+      loc.longitude = loc.loc.split(',')[1];
+    }
+  }
+
   var map = new GMaps({
     div: '#map',
     lat: loc.latitude,
